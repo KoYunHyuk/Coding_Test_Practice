ec0a9dc243b7b9b47b1bf3eb70e7d58a22b5502f
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2022-02-03 03:13:09 +0900

    Complete 1003 피보나치 함수

--- Class 3/1003/피보나치 함수.cpp
+++ Class 3/1003/피보나치 함수.cpp
@@ -2,13 +2,25 @@
 
 using namespace std;
 
+int dp[50] = { 0, 1, };
+
+int fibonachi(int n) {
+	if (n == 0 || n == 1) {
+		return dp[n];
+	}
+	else if (dp[n] == 0) {
+		dp[n] = fibonachi(n - 1) + fibonachi(n - 2);
+	}
+	return dp[n];
+}
+
 int main() {
 	int N;
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		cout << i << endl;
+	int num;
+
+	for (int i = 0; i < 10; i++) {
+		cout << fibonachi(i) << endl;
 	}
-	
-	
 }
