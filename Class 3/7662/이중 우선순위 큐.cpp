7f910fab5a9623bb7e81b79f78f71dfdc7507f00
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2022-05-12 00:08:50 +0900

    Complete (G4) 7662. 이중 우선순위 큐 - Max/Min Heap 방법

--- Class 3/7662/이중 우선순위 큐.cpp
+++ Class 3/7662/이중 우선순위 큐.cpp
@@ -1,18 +1,72 @@
 #include <iostream>
 #include <set>
+#include <queue>
 
 using namespace std; // G4
 
 int T, k;
 string str;
+bool visited[1000001];
 
 int main() {
-	cin >> T;
 	char c;
 	int n;
+
+	cin >> T;
 	while (T--) {
 		cin >> k;
-		multiset<int> ms;
+
+		// 1. max_heap, min_heap 이용
+		priority_queue<pair<int, int>> max_heap;
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+
+		for (int i = 0; i < k; i++) {
+			cin >> c >> n;
+			if (c == 'I') {
+				max_heap.push({ n, i });
+				min_heap.push({ n, i });
+				visited[i] = true;
+			}
+			else {
+				if (n == 1) { 
+					while (!max_heap.empty() && !visited[max_heap.top().second]) {
+						max_heap.pop();
+					}
+
+					if (!max_heap.empty()) {
+						visited[max_heap.top().second] = false;
+						max_heap.pop();
+					}
+				}
+				else {
+					while (!min_heap.empty() && !visited[min_heap.top().second]) {
+						min_heap.pop();
+					}
+
+					if (!min_heap.empty()) {
+						visited[min_heap.top().second] = false;
+						min_heap.pop();
+					}
+				}
+			}
+		}
+
+		while (!max_heap.empty() && !visited[max_heap.top().second]) {
+			max_heap.pop();
+		}
+		while (!min_heap.empty() && !visited[min_heap.top().second]) {
+			min_heap.pop();
+		}
+
+		if (min_heap.empty() && max_heap.empty()) {
+			cout << "EMPTY\n";
+		}
+		else {
+			cout << max_heap.top().first << " " << min_heap.top().first << "\n";
+		}
+
+		// 2. multiset 이용
+		/*multiset<int> ms;
 
 		while (k--) {
 			cin >> c >> n;
@@ -38,7 +92,6 @@ int main() {
 			auto end_iter = ms.end();
 			end_iter--;
 			cout << *end_iter << " " << *ms.begin() << "\n";
-		}
-
+		}*/
 	}
 }
